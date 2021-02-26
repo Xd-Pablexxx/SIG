a21698f2b255895618b0a9a8aa8a76557e136a35
Xd-Pablexxx <68670729+Xd-Pablexxx@users.noreply.github.com>
2021-02-26 10:44:30 -0500

    ok

--- index2.php
+++ index2.php
@@ -6,9 +6,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+    <link rel="icon" href="images/icon.ico" type="image/ico" />
 
-    <title>Gentelella Alela! | </title>
+<title>FastBUY</title>
 
     <!-- Bootstrap -->
     <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -36,27 +36,21 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="index.php" class="site_title"><i class="fa fa-paw"></i> <span>Proyecto Sig</span></a>
+            <a href="index.php" class="site_title">
+                <img src="images/icon.ico" alt="..." width="60" height="50"><span> FastBUY</span></a>
+                <br>
             </div>
 
             <div class="clearfix"></div>
 
             <!-- menu profile quick info -->
-            <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Bienvenido</span>
-                <h2>Usuario</h2>
-              </div>
-            </div>
+           
             <!-- /menu profile quick info -->
 
             <br />
 
             <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu" style="margin-top: 20px;">
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
